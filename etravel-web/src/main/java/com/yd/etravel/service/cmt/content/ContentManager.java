8d0a2f13e87a8553cd5a5fc8493eccce6889c777
shanakaray <shanakaray@2a276eac-e233-a409-7036-f72561b38558>
2012-06-17 14:21:52 +0000

    add content service

---
+++ etravel-web/src/main/java/com/yd/etravel/service/cmt/content/ContentManager.java
@@ -0,0 +1,77 @@
+package com.yd.etravel.service.cmt.content;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yd.etravel.domain.cmt.content.Image;
+import com.yd.etravel.persistence.dao.cmt.content.IImageDAO;
+import com.yd.etravel.persistence.exception.PersistenceException;
+import com.yd.etravel.service.exception.ServiceException;
+
+@Service(value = "contentService")
+@Transactional(propagation = Propagation.SUPPORTS)
+public class ContentManager implements IContentManager {
+    @Autowired(required = true)
+    private IImageDAO iImageDAO;
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Image saveImage(final Image image) throws ServiceException {
+	try {
+	    this.iImageDAO.save(image);
+	} catch (final PersistenceException exception) {
+	    throw new ServiceException(null, exception);
+	}
+	return image;
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    public Image getImage(final Long id) throws ServiceException {
+	Image image = null;
+	FileOutputStream fos = null;
+	try {
+	    image = this.iImageDAO.findById(id);
+	    final File img = new File(System.getProperty("java.io.tmpdir")
+		    + image.getId() + image.getCode());
+	    if (!img.exists()) {
+		final byte[] bytes = image.getSource();
+		fos = new FileOutputStream(System.getProperty("java.io.tmpdir")
+			+ image.getId() + image.getCode());
+		fos.write(bytes);
+		// /
+	    }
+	    image.setFile(img);
+	} catch (final Exception exception) {
+	    throw new ServiceException(null, exception);
+	} finally {
+	    try {
+		if (fos != null) {
+		    fos.close();
+		}
+	    } catch (final IOException e) {
+		e.printStackTrace();
+	    }
+	}
+	return image;
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    public List<Long> getImageList(final Image image) throws ServiceException {
+	try {
+	    return this.iImageDAO.getIds(image);
+	} catch (final PersistenceException exception) {
+	    throw new ServiceException(null, exception);
+	}
+
+    }
+
+}
---
+++ etravel-web/src/main/java/com/yd/etravel/service/cmt/content/IContentManager.java
@@ -0,0 +1,16 @@
+package com.yd.etravel.service.cmt.content;
+
+import java.util.List;
+
+import com.yd.etravel.domain.cmt.content.Image;
+import com.yd.etravel.service.exception.ServiceException;
+
+public interface IContentManager {
+
+    public Image saveImage(Image image) throws ServiceException;
+
+    public Image getImage(Long id) throws ServiceException;
+
+    public List<Long> getImageList(Image image) throws ServiceException;
+
+}
